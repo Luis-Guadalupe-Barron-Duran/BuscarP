8a13995414601be8516d04572f4c641a606bac39
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2026-02-14 20:53:37 -0600

    Variables de mi main conectadas con el index para generar las funciones de los botones y mas

---
+++ front/main.js
@@ -0,0 +1,9 @@
+const input = document.getElementById('input');
+const buscarbtn = document.getElementById('buscarbtn');
+const clearbtn = document.getElementById('clearbtn');
+const tablaproductos = document.getElementById('tablaproductos');
+const status = document.getElementById('status');
+const encontrados = document.getElementById('encontrados');
+
+const url = 'http://localhost:4000/api';
+
